089e4eff6cb8a2ff755e29c5136c9217d76cc037
Lucasemotta <lucasgeek19@gmail.com>
2026-08-18 20:35:09 -0300

    Padroes de codigo linha

---
+++ main.py
@@ -0,0 +1,15 @@
+#x=10
+#y=20
+#z=x+y 
+#print(z) 
+
+#nome = "Lucas"
+#idade = 21 
+#
+#if idade >= 18: 
+#    print ("Maior de idade ")
+
+preco_produto = 10 
+quantidade_produto=5 
+total = preco_produto + quantidade_produto
+print ("total da commpra foi: " + str(total) ) 
